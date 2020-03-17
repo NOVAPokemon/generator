553cb15c3ee239960b504823dcc360b444c76d14
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-03-17 17:40:01 +0000

    Change generation to only insert in DB

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/NOVAPokemon/generator
 go 1.13
 
 require (
-	github.com/NOVAPokemon/authentication v0.0.0-20200316161255-0d4c97ca40aa // indirect
-	github.com/NOVAPokemon/utils v0.0.2
+	github.com/NOVAPokemon/utils v0.0.13
 	github.com/sirupsen/logrus v1.4.2
 )
--- main.go
+++ main.go
@@ -1,41 +1,86 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/NOVAPokemon/utils"
+	"github.com/NOVAPokemon/utils/database/pokemon"
 	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"io/ioutil"
 	"math/rand"
-	"net/http"
 	"time"
 )
 
-const host = "localhost"
-const Port = 8002
+const PokemonsFile = "pokemons.json"
+const MaxLevel = 100
+const MaxHP = 500
+const MaxDamage = 250
 
-var (
-	NumberOfItems    int
-	NumberOfPokemons int
-)
+const numberOfPokemonsToGenerate = 10
+const intervalBetweenGenerations = 2 * time.Minute
+
+var pokemonSpecies = loadPokemons()
 
 func main() {
 	rand.Seed(time.Now().Unix())
 
-	NumberOfPokemons = getNumberOfPokemons()
-	NumberOfItems = getNumberOfItems()
+	log.Infof("Starting GENERATOR server...\n")
 
-	router := utils.NewRouter(routes)
-	addr := fmt.Sprintf("%s:%d", host, Port)
-
-	log.Infof("Starting GENERATOR server in port %d...\n", Port)
-	log.Fatal(http.ListenAndServe(addr, router))
+	for {
+		log.Info("Refreshing wild pokemons...")
+		cleanWildPokemons()
+		generateWildPokemons()
+		time.Sleep(intervalBetweenGenerations)
+	}
 }
 
-func getNumberOfPokemons() int {
-	// TODO change this for mongoDB call to size of collection
-	return 20
+func cleanWildPokemons() {
+	err := pokemon.DeleteWildPokemons()
+
+	if err != nil {
+		return
+	}
+}
+
+func generateWildPokemons() {
+	for i := 0; i < numberOfPokemonsToGenerate; i++ {
+		toAdd := utils.Pokemon{
+			Id:      primitive.NewObjectID(),
+			Owner:   primitive.NewObjectID(),
+			Species: pokemonSpecies[rand.Intn(len(pokemonSpecies))],
+			Level:   rand.Intn(MaxLevel),
+			HP:      rand.Intn(MaxHP),
+			Damage:  rand.Intn(MaxDamage),
+		}
+
+		err, _ := pokemon.AddWildPokemon(toAdd)
+
+		if err != nil {
+			log.Error("Error adding wild pokemon")
+			log.Error(err)
+		}
+	}
 }
 
 func getNumberOfItems() int {
 	// TODO change this for mongoDB call to size of collection
 	return 20
+}
+
+func loadPokemons() []string {
+	data, err := ioutil.ReadFile(PokemonsFile)
+	if err != nil {
+		log.Fatal("Error loading pokemons file")
+		return nil
+	}
+
+	var pokemonNames []string
+	err = json.Unmarshal(data, &pokemonNames)
+
+	if err != nil {
+		log.Errorf("Error unmarshalling pokemons name")
+		log.Fatal(err)
+	}
+
+	return pokemonNames
 }
